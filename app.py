0ee051315442614e7c0efaab6f8a634695675a9c
Nathan-Austin <nath.austin@gmail.com>
2022-11-08 14:31:49 +0100

    movie recommneder with random recommneder added

---
+++ app.py
@@ -0,0 +1,22 @@
+from recommender import recommend_random
+from utils import movies
+from flask import Flask,render_template,request
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template('index.html',name = 'Nathan',movies=movies['title'].to_list())
+
+@app.route('/recommendation')
+def recommendation():
+    print(request.args)
+    titles = request.args.getlist('input')
+    ratings = request.args.getlist('rating')
+    user_rating = dict(zip(titles,ratings))
+    recs = recommend_random(movies, user_rating,k=3)
+    return render_template('recommendation.html',recs=recs)
+
+if __name__=='__main__': 
+    app.run(debug=True)
+
