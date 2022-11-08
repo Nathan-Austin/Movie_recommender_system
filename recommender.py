7bbc9df4f796c41efb56e58cb25a6ccdfa45cfa6
Nathan-Austin <nath.austin@gmail.com>
2022-11-08 21:04:33 +0100

    test NMF fuction added

--- recommender.py
+++ recommender.py
@@ -26,7 +26,41 @@ def recommend_random(movies, user_rating, k=5):
     random_movies = np.random.choice(list(recommend.index), replace=False, size=k)
     return random_movies  
 
+def recommend_test_nmf(movies, user_rating, model, k=5):
+    """
+    return k random unseen movies for user 
+    """
+    """
+    Filters and recommends the top k movies for any given input query based on a trained NMF model. 
+    Returns a list of k movie ids.
+    """
+    # 1. candiate generation
+    # construct a user vector
+ 
+    data = list(user_rating.values())   # the ratings of the new user
+    row_ind = [0]*len(data)       # we use just a single row 0 for this user 
+    col_ind = list(user_rating.keys())  # the columns (=movieId) of the ratings
+    # R.shape[1] = 168253
+    user_vec = csr_matrix((data, (row_ind, col_ind)), shape=(1, 168253))
+   
+    # 3. scoring
+    # calculate the score with the NMF model
+    scores = model.inverse_transform(model.transform(user_vec))
+    # convert to a pandas series
+    scores = pd.Series(scores[0])
 
+    # 4. ranking
+
+    # filter out movies allready seen by the user
+    # give a zero score to movies the user has already seen
+    scores[user_rating.keys()] = 0
+    # sort the scores from high to low 
+    scores = scores.sort_values(ascending=False)
+    
+    # return the top-k highst rated movie ids or titles
+    recommendations = scores.head(k).index
+    recommendations = movies.loc[recommendations]
+    return list(recommendations["title"])
 
 def recommend_with_NMF(user_item_matrix, user_rating, model, k=5):
     """
